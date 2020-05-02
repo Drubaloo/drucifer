27bb89f682e1bd289c2d1ee4def912ffb7a2630a
Drubaloo <Drubaloo@outlook.com>
2020-05-01 19:59:26 -0700

    moving to my desktop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
 import React from 'react';
 import './App.css';
 import './resources/js/title'
+import { ParallaxProvider, Parallax } from 'react-skrollr'
 
 function App() {
   return (
+    
     <div className="App">
-      <div className="container">
-        <h1>Dru Sanchez
+      <ParallaxProvider>
+          <Parallax
+            data={{
+              'data-center-center': 'opacity: 1;',
+              'data-bottom-top': 'opacity: 0;'
+            }}
+          >
+            Some content or Component
+      <div className="img1">
+        <div className="container1">
+          <div className="title">
+            <h1>Dru Sanchez
   <span
-            class="txt-rotate"
-            data-period="2000"
-            data-rotate='[ " Motivated.", " Determined.", " Enthusiatstic.", " Quick to learn."]'></span>
-        </h1>
-
-        <div className="img1">
-          
+                class="txt-rotate"
+                data-period="2000"
+                data-rotate='[ " Motivated.", " Determined.", " Enthusiatstic.", " Quick to learn."]'></span>
+            </h1>
+          </div>
+some test
         </div>
 
       </div>
 
 
+                </Parallax>
+              </ParallaxProvider>
 
 
     </div>
